4180c9c62278c6f76be438c26b3b04dbbaba81d8
Damilolaadefenwa <ade24039@byui.edu>
2025-05-16 11:29:11 +0100

    new updates

--- week02/scripts/getbom.js
+++ week02/scripts/getbom.js
@@ -5,7 +5,7 @@
 
 const input = document.querySelector('#favchapter');
 const button = document.querySelector('button');
-const list = Document.querySelector('__________'); //you need to fill in the blank here to reference the HTML element that is a unordered list element.
+const list = Document.querySelector('#list'); //you need to fill in the blank here to reference the HTML element that is a unordered list element.
 
 //6.Create a li element that will hold each entry's chapter title and an associated delete button.
 
@@ -29,5 +29,29 @@ list.append(li); //this will append the li element to the unordered list in your
 
 
 //Activity Instructions 2 HANDLING DOM EVENTS
+//1 open file and 2. Create a click event listener for the Add Chapter button in the html using an addEventListener.
+//3.Within the Add Chapter button click event function block,do the following:
+button.addEventListener('click', function () {
+    //code to execute when the button is clicked
+    const addChapter = input.value.trim(); //this will be the value of the input element
+    if (addChapter !== "") {
+        const li = document.createElement('li'); //this will hold the chapter title and delete button
+        li.textContent = addChapter; //this will be the text on the li element
 
+        const deleteButton = document.createElement('button'); //this will be the delete button
+        deleteButton.textContent = '❌'; //this will be the text on the delete button
+        deleteButton.addEventListener('click', () => {
+            list.removeChild(li); //this will remove the li element from the unordered list
+            input.focus(); //this will set the focus back to the input element.
+        });
+
+        li.appendChild(deleteButton); //this will append the delete button to the li element
+        list.appendChild(li); //this will append the li element to the unordered list in your HTML
+        input.value = ""; //this will clear the input element //13.Change the input value to clean up the interface for the user.
+        input.focus(); //this will set the focus back to the input element.
+    }
+    // else {
+    //     alert("Please enter a chapter title."); //this will alert the user to enter a chapter title
+    // }
+});
 

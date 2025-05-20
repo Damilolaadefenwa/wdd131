3816a9d80916e0783ae7e0f95938c85aa9d42a0e
Damilolaadefenwa <ade24039@byui.edu>
2025-05-20 16:15:28 +0100

    last edits commit

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,4 +1,4 @@
-/* Start with the Base script used for previous footer */
+/* 1 step: Start with the Base script used for previous footer */
 // Get the current year for the copyright
 const copyrightYearSpan = document.getElementById("current-year");
 const currentYear = new Date().getFullYear();
@@ -9,9 +9,11 @@ const lastModifiedSpan = document.getElementById("last-modified");
 const lastModifiedDate = document.lastModified;
 lastModifiedSpan.textContent = lastModifiedDate;
 
+// 2 step: Write code for the hamburger menu interation
+// create variable first and target your selector
 const hamButton = document.querySelector('#menu');
 const navElement = document.querySelector('nav');
-
+//Use Eventlistiener to fire an action for interactivity
 hamButton.addEventListener('click', () => {
     navElement.classList.toggle('open');
     hamButton.classList.toggle('open');

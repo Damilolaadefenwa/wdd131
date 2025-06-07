eed31d2417c916048fa436379d31c362494eb5f5
Damilolaadefenwa <ade24039@byui.edu>
2025-06-07 22:24:15 +0100

    first commit

---
+++ week05/scripts/form.js
@@ -0,0 +1,10 @@
+/* 1 step: Start with the Base script used for previous footer */
+// Get the current year for the copyright
+const copyrightYearSpan = document.getElementById("current-year");
+const currentYear = new Date().getFullYear();
+copyrightYearSpan.textContent = currentYear;
+
+// Get the last modified date of the document
+const lastModifiedSpan = document.getElementById("last-modified");
+const lastModifiedDate = document.lastModified;
+lastModifiedSpan.textContent = lastModifiedDate;
